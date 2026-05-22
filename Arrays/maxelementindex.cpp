3aca829c14555e02bc76175414e80c4801f959b7
Anjali <anjalikasar06@gmail.com>
2026-05-22 14:07:39 +0530

    solved maxelement index

---
+++ Arrays/maxelementindex.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cout<<"size:";
+  cin>>n;
+
+  vector<int> arr(n);
+
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+
+  int max =arr[0];
+
+  for(int i=0;i<n;i++){
+    if(arr[i]>max){
+      max=arr[i];
+    }
+  }
+
+  cout<<max;
+  
+}
